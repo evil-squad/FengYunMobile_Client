83a7a99808bdeed9986c376cd0188dd73d43380e
blucenong <351536411@163.com>
2017-01-16 22:55:22 +0800

    Add MathTypes File.

---
+++ FengYun/frameworks/runtime-src/Classes/MathTypes.h
@@ -0,0 +1,26 @@
+/**
+ * @file  MathTypes.h
+ * @project  FengYun
+ * @author Bluce Nong <nongjinxia@mokylin.com>
+ * @Created by BluceNong on 16/01/2017.
+ * @  Copyright © 2017 MokyLin  Bluce Nong <nongjinxia@mokylin.com>. All rights reserved.
+*/
+
+#ifndef MATH_TYPES_H
+#define MATH_TYPES_H
+
+#include "cocos2d.h"
+#include "gamebase.h"
+
+BEGIN_NS_FY
+    typedef cocos2d::Vec2 Vector2;
+    typedef cocos2d::Vec3 Vector3;
+    typedef cocos2d::Vec4 Vector4;
+    typedef cocos2d::Quaternion Quaternion;
+    typedef cocos2d::Size Size;
+    typedef cocos2d::Rect Rect;
+    typedef cocos2d::Color3B Color3B;
+    typedef cocos2d::Color4B Color4B;
+END_NS_FY
+
+#endif /* MATH_TYPES_H */
