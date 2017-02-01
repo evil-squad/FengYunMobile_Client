654e9bd5874edb1f9d436b378fe87181ed85465a
blucenong <351536411@163.com>
2017-02-01 16:30:16 +0800

    Modify Role.

--- FengYun/frameworks/runtime-src/Classes/ECS/Role.cpp
+++ FengYun/frameworks/runtime-src/Classes/ECS/Role.cpp
@@ -7,8 +7,113 @@
 */
 
 #include "Role.h"
+#include "Entity.h"
+#include "FSM/Fsm.h"
+#include "FSM/BaseData.h"
+#include "Renderer.h"
 
 BEGIN_NS_ECS
+
+static const float ANGLE_OFFSET = 22.5;
+static const float PLAYER_RUN_SPEED = 100;
+
+namespace
+{
+    static FaceDir changeFaceDir(float angle)
+    {
+        if (angle <= -90 + ANGLE_OFFSET && angle > -90 - ANGLE_OFFSET)
+            return FaceDir::FRONT;
+        else if (angle <= -45 + ANGLE_OFFSET && angle > -45 - ANGLE_OFFSET)
+            return FaceDir::RIGHT_DOWN;
+        else if ( angle <= 0 + ANGLE_OFFSET && angle > 0 - ANGLE_OFFSET)
+            return FaceDir::RIGHT;
+        else if (angle <= 45 + ANGLE_OFFSET && angle > 45 - ANGLE_OFFSET)
+            return FaceDir::RIGHT_UP;
+        else if (angle <= 90 + ANGLE_OFFSET && angle > 90 - ANGLE_OFFSET)
+            return FaceDir::BACK;
+        else if (angle <= 135 + ANGLE_OFFSET && angle > 135 - ANGLE_OFFSET)
+            return FaceDir::LEFT_UP;
+        else if ((angle <= 180 && angle > 180 - ANGLE_OFFSET) || (angle <= -180 + ANGLE_OFFSET && angle > -180))
+            return FaceDir::LEFT;
+        else if (angle <= -135 + ANGLE_OFFSET && angle > -135 - ANGLE_OFFSET)
+            return FaceDir::LEFT_DOWN;
+
+        return FaceDir::NONE;
+    }
+}
+
+/////////////////TODO: class Role
+Role::Role(const ComponentType<Component>& type, RoleType roleType)
+    : Component(type)
+    , _roleType(roleType)
+    , _faceDir(FaceDir::FRONT)
+    , _fsm(nullptr)
+    , _fsmData(nullptr)
+    , _avatar(nullptr)
+    , _name("")
+{
+}
+
+Role::~Role()
+{
+}
+
+bool Role::init(const std::string &name, bool hasFsm)
+{
+    _name = name;
+
+    auto ren = getEntity()->getComponent<Renderer>();
+    if (!ren)
+        ren = getEntity()->addComponent<Renderer>();
+
+    _avatar = getEntity()->addComponent<Avatar>();
+
+    auto node = ren->getDisplayNode();
+
+    _avatar->load("avatar/man", "man");
+
+    auto a = _avatar->getDisplayNode();
+    node->addChild(a, 10);
+    
+    if (hasFsm)
+    {
+        _fsm = new fsm::Fsm();
+        _fsmData = new fsm::BaseData();
+
+        _fsmData->setRole(ComponentHandle<Role>(this));
+        _fsmData->setMotor(new fsm::Motor(_fsmData->getRole()));
+    }
+    return true;
+}
+
+void Role::setFaceDir(fy::FaceDir dir)
+{
+    _faceDir = dir;
+}
+
+const ComponentHandle<Transform>& Role::getTransform()
+{
+    return getEntity()->getTransform();
+}
+
+void Role::onUpdate(float dt)
+{
+}
+
+void Role::onReceive(const Event<void> &ev)
+{
+
+}
+
+void Role::onStart()
+{
+
+}
+
+void Role::onDestroy()
+{
+    
+}
 
 
 END_NS_ECS
--- FengYun/frameworks/runtime-src/Classes/ECS/Role.h
+++ FengYun/frameworks/runtime-src/Classes/ECS/Role.h
@@ -14,16 +14,74 @@
 #include "Component.h"
 #include "Event.h"
 #include "Transform.h"
+#include "Avatar.h"
+
+namespace fy
+{
+    namespace fsm
+    {
+        class Fsm;
+        class BaseData;
+    }
+
+    namespace net
+    {
+        class AgentMsg;
+    }
+
+    namespace scenes
+    {
+        class GameScene;
+    }
+}
 
 BEGIN_NS_ECS
 
+class SceneAgentEvent : public Event<SceneAgentEvent>
+{
+public:
+    static const char* getTypeName() { return "SceneAgentEvent"; }
+
+    SceneAgentEvent(const net::AgentMsg& msg_)
+        : msg(msg_)
+    {}
+
+    const net::AgentMsg& msg;
+};
+
 class Role : public Component
 {
-private:
+public:
+    RoleType getRoleType() const { return _roleType; }
+    void setRoleType(RoleType type) { _roleType = type; }
+
     FaceDir getFaceDir() const { return _faceDir; }
-    void setFaceDir(FaceDir);
+    void setFaceDir(FaceDir dir);
+
+    const ComponentHandle<Transform>& getTransform();
+    const std::string& getName() const { return _name; }
+
+    const ComponentHandle<Avatar>& getAvatar() { return _avatar; }
+    scenes::GameScene* getScene() { return getWorldUserData<scenes::GameScene>(); }
+protected:
+    Role(const ComponentType<Component>& type, RoleType roleType);
+    virtual ~Role();
+    bool init(const std::string& name, bool hasFsm);
+
+    virtual void onUpdate(float dt) override;
+    virtual void onReceive(const Event<void>& ev) override;
+
+    virtual void onStart() override;
+    virtual void onDestroy() override;
+
+    fsm::Fsm* _fsm;
+    fsm::BaseData* _fsmData;
+    ComponentHandle<Avatar> _avatar;
+
 private:
     FaceDir _faceDir;
+    RoleType _roleType;
+    std::string _name;
 };
 
 template <>
