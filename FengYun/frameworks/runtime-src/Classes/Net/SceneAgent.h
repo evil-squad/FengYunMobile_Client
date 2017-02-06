3c8e5fd68329acc48aa9997fa2af1acc8fa83af5
blucenong <351536411@163.com>
2017-02-06 12:01:53 +0800

    Add Scene Agent data.

--- FengYun/frameworks/runtime-src/Classes/Net/SceneAgent.h
+++ FengYun/frameworks/runtime-src/Classes/Net/SceneAgent.h
@@ -21,7 +21,69 @@ BEGIN_NS_NET
 
 enum class AgentMsgType
 {
-    MOVE
+    MOVE,
+    MOVE_TYPE_CHANGE,
+    STOP_MOVE,
+    JUMP,
+    RELEASE_SPELL,
+    SPELL_ATTACK,
+    STANDING,
+    NAV_PATH
+};
+
+struct CommondAgentData
+{
+    int id;
+    int direct;
+    int posX;
+    int posY;
+    int posZ;
+    Vector3 speed;
+    Vector3 step;
+};
+
+struct PlayerAgentData : CommondAgentData
+{
+
+};
+
+struct MonsterAgentData : CommondAgentData
+{
+
+};
+
+struct AgentStandingData
+{
+    int id;
+    int posX;
+    int posY;
+    int posZ;
+    static AgentMsgType msg_type() { return AgentMsgType::STANDING; }
+};
+
+struct AgentMoveData
+{
+    int id;
+    int posX;
+    int posY;
+    int posZ;
+    Vector3 speed;
+    Vector3 step;
+
+    AgentMoveData()
+    {}
+
+    static AgentMsgType msg_type() { return AgentMsgType::MOVE; }
+};
+
+struct AgentStopMoveData
+{
+    int id;
+    int posX;
+    int posY;
+    int posZ;
+
+    static AgentMsgType msg_type() { return AgentMsgType::STOP_MOVE; }
 };
 
 class AgentMsg
