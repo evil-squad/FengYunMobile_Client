8606e88fce57a4d163b6c9e7add548186385fea2
blucenong <351536411@163.com>
2017-02-07 12:03:52 +0800

    Commit game-enem.rb about enum joystickdir.

--- FengYun/tools/lua-enum-generator/game-enum.rb
+++ FengYun/tools/lua-enum-generator/game-enum.rb
@@ -12,6 +12,18 @@ Builder.define do
 			value AXIS_EVENT, 1
 			value BUTTON_EVENT
 		end
+
+		enum JoystickDir do
+			value NONE, 0
+			value FRONT
+			value RIGHT_DOWN
+			value RIGHT
+			value RIGHT_UP
+			value BACK
+			value LEFT_UP
+			value LEFT
+			value LEFT_DOWN
+		end
 	end
 
 	output("../../src/GameConstants.lua")
