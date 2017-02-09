3e3e489e984f1ab6946d5ba5085ea17e5473a404
blucenong <351536411@163.com>
2017-02-09 14:09:03 +0800

    Modify UserFreeMover Update Infos.

--- FengYun/frameworks/runtime-src/Classes/FSM/UserFreeMover.cpp
+++ FengYun/frameworks/runtime-src/Classes/FSM/UserFreeMover.cpp
@@ -10,6 +10,7 @@
 #include "BaseState.h"
 #include "NavSystem.h"
 #include "MapManager.h"
+#include "Moving.h"
 
 BEGIN_NS_FSM
 
@@ -56,10 +57,55 @@ void UserFreeMover::adjustAxis(fy::fsm::BaseState *st, float dt)
 
 void UserFreeMover::updatePredicts(fy::fsm::BaseState *st, bool force)
 {
+    if (_serverMoving && _adjustAxis.isZero())
+    {
+        _serverMoving = false;
+        auto role = st->getBaseData()->getRole();
+        auto c = role->getScene()->getBattleController();
+        if (c)
+            c->onStopMove(role);
+        return ;
+    }
+
+    auto v = _adjustAxis.getNormalized();
+    auto vv = Vector3(v.x, v.y, 0);
+
+    Vector3 lastWalkablePoint;
+    auto pos = st->getBaseData()->getRole()->getTransform()->getPosition();
+    auto curMapPos = MapManager::toMapPoint(pos);
+    lastWalkablePoint = curMapPos;
+    int mapGridSize = MapManager::gridSize();
+    for (int i = 0; i < 8; ++ i)
+    {
+        pos += vv * mapGridSize;
+        Vector3 mapPos = MapManager::toMapPoint(pos);
+
+        if (NavSystem::getInstance()->isGround(mapPos) &&
+            !NavSystem::getInstance()->isWalkable(mapPos))
+            break;
+
+        lastWalkablePoint = mapPos;
+    }
+
+    if (force || _predictPoint != lastWalkablePoint)
+    {
+        _predictPoint = lastWalkablePoint;
+        Vector3 step = lastWalkablePoint - curMapPos;
+        if (!step.isZero())
+        {
+            _serverMoving = true;
+            auto role = st->getBaseData()->getRole();
+            auto c = role->getScene()->getBattleController();
+            if (c)
+                c->onMove(role, _running, step);
+        }
+    }
 }
 
 void UserFreeMover::updateSpeed(fy::fsm::BaseState *st)
 {
+    int gridSize = MapManager::gridSize();
+    _speed = ( _running ? 80 : 60) * gridSize / 10;
 }
 
 void UserFreeMover::enter(BaseState* st)
@@ -87,13 +133,55 @@ void UserFreeMover::exit(fy::fsm::BaseState *st)
     {
         _serverMoving = false;
         auto role = st->getBaseData()->getRole();
-        
+        auto c = role->getScene()->getBattleController();
+        if (c)
+            c->onStopMove(role);
     }
+
+    _axis = Vector2::ZERO;
+    _adjustAxis = Vector2::ZERO;
+    _lastAdjustAxis = Vector2::ZERO;
+    _predictPoint = Vector3::ZERO;
 }
 
 void UserFreeMover::update(fy::fsm::BaseState *st)
 {
-    
+    updateSpeed(st);
+    adjustAxis(st, st->getFsm()->getDeltaTime());
+
+    if (_isNormalMoving)
+    {
+        _time += st->getFsm()->getDeltaTime();
+        if (!_running && _time > AccelTime)
+        {
+            static_cast<Moving*>(st)->startRun();
+            _running = true;
+
+            updatePredicts(st, true);
+        }
+    }
+
+    if (_lastAdjustAxis != _adjustAxis)
+    {
+        _lastAdjustAxis = _adjustAxis;
+        updatePredicts(st);
+    }
+
+    if (_serverMoving)
+    {
+        auto& pos = st->getBaseData()->getRole()->getTransform()->getPosition();
+        auto mapPos = MapManager::toMapPoint(pos);
+
+        if (mapPos == _predictPoint)
+        {
+            updatePredicts(st);
+        }
+
+        auto v = _adjustAxis.getNormalized() * _speed;
+        auto motor = st->getBaseData()->getMotor();
+        motor->setSpeed(Vector3(v.x, v.y, 0));
+        motor->move(st->getFsm()->getDeltaTime());
+    }
 }
 
 
