469a66bfa0bc26bef09c5296a36f2bf2e2468aaa
blucenong <351536411@163.com>
2017-02-15 16:48:24 +0800

    Add NetManager File.

---
+++ FengYun/frameworks/runtime-src/Classes/managers/NetManager.cpp
@@ -0,0 +1,233 @@
+/**
+ * @file  NetManager.cpp
+ * @project  FengYun
+ * @author Bluce Nong <nongjinxia@mokylin.com>
+ * @Created by BluceNong on 2017/2/13.
+ * @  Copyright © 2017年 MokyLin  Bluce Nong <nongjinxia@mokylin.com>. All rights reserved.
+*/
+
+#include <queue>
+#include <functional>
+
+#include "network/HttpClient.h"
+
+#define ASIO_STANDALONE 1
+#include "asio.hpp"
+
+#include "NetManager.h"
+#include <thread>
+
+#include "DebugHelper.h"
+#include "GameApp.h"
+#include "cocos2d.h"
+
+using asio::ip::tcp;
+
+using namespace cocos2d::network;
+
+BEGIN_NS_FY
+
+#include "Net/TcpConnection.ipp"
+#include "Net/Timer.ipp"
+
+struct NetManager::PrivateData
+{
+    std::thread thread;
+    asio::io_service io;
+
+    net::GameConnection* gameConnection;
+    std::function<void()> lostCallback;
+    std::vector<Listener*> listeners;
+
+    asio::io_service::work* work;
+
+    PrivateData() : gameConnection(nullptr), work(nullptr)
+    {
+        work = new asio::io_service::work(io);
+        thread = std::thread([this]{
+            io.run();
+        });
+    }
+
+    ~PrivateData()
+    {
+        if (gameConnection) delete gameConnection;
+        delete work;
+        if (thread.joinable()) thread.join();
+    }
+
+    void addListener(Listener* l)
+    {
+        listeners.push_back(l);
+    }
+
+    void removeListener(Listener* l)
+    {
+        auto iter = std::find(listeners.begin(), listeners.end(), l);
+        if (iter != listeners.end()) listeners.erase(iter);
+    }
+};
+
+NetManager::NetManager()
+{
+    _data = new PrivateData();
+}
+
+NetManager::~NetManager()
+{
+    delete _data;
+}
+
+void NetManager::setLostCallback(const std::function<void()>& cb)
+{
+    _data->lostCallback = cb;
+}
+
+void NetManager::connect(const std::string &name, const std::string &ip, unsigned short port, const std::function<void (bool)> &cb)
+{
+    if (name == "GAME_SERVER")
+    {
+        auto conn = new TcpConnection(_data->io);
+
+        auto endpoint = asio::ip::tcp::endpoint(asio::ip::address::from_string(ip), port);
+        conn->getSocket().async_connect(endpoint, [this, conn, cb](const asio::error_code& ec){
+            if (ec)
+            {
+                conn->destroy();
+                GameApp::runInMainThread([=]{
+                    cb(false);
+                });
+                return ;
+            }
+
+            conn->getSocket().non_blocking(true);
+            conn->getSocket().set_option(asio::ip::tcp::no_delay(true));
+            auto gconn = new net::GameConnection(conn);
+            _data->gameConnection = gconn;
+            _data->gameConnection->setLostCallback([=] {
+                GameApp::runInMainThread([=]{
+                    if (_data->gameConnection != gconn) return ;
+                    disconnect("GAME_SERVER");
+                    auto cb = _data->lostCallback;
+                    if (cb) cb();
+                });
+            });
+
+            GameApp::runInMainThread([=]{
+                for (auto l: _data->listeners) l->onConnected("GAME_SERVER");
+
+                gconn->start();
+                cocos2d::Director::getInstance()->getScheduler()->schedule([gconn](float dt){
+                    gconn->dispatchAll();
+                }, this, 0, false, "game_conn_dispatch");
+                cb(true);
+            });
+        });
+    }
+}
+
+void NetManager::disconnect(const std::string &name)
+{
+    if (name == "GAME_SERVER")
+    {
+        if (_data->gameConnection)
+        {
+            cocos2d::Director::getInstance()->getScheduler()->unschedule("game_conn_dispatch", this);
+
+            for (auto l : _data->listeners)
+            {
+                l->onDisconnected("GAME_SERVER");
+            }
+
+            delete _data->gameConnection;
+            _data->gameConnection = nullptr;
+        }
+    }
+}
+
+net::GameConnection* NetManager::getGameConnection()
+{
+    return _data->gameConnection;
+}
+
+void NetManager::registerListener(fy::NetManager::Listener *listener)
+{
+    _data->addListener(listener);
+}
+
+void NetManager::unregisterListener(fy::NetManager::Listener *listener)
+{
+    _data->removeListener(listener);
+}
+
+net::Timer* NetManager::createTimer()
+{
+    return new Timer(_data->io);
+}
+
+HttpRequest* createJsonRequest(const std::string& url, const std::string& postStr)
+{
+    HttpRequest* request = new HttpRequest();
+    request->setUrl(url);
+
+    if (postStr.empty())
+    {
+        request->setRequestType(cocos2d::network::HttpRequest::Type::GET);
+    }
+    else
+    {
+        request->setRequestType(cocos2d::network::HttpRequest::Type::POST);
+        request->setRequestData(postStr.c_str(), postStr.size() + 1);
+    }
+
+    std::vector<std::string> headers;
+    headers.push_back("Content-Type: application/json; charset=utf-8");
+    request->setHeaders(headers);
+
+    return request;
+}
+
+void onReceiveJson(HttpClient* client, HttpResponse* response, std::function<void(bool succ, const std::string& respStr)>& callback)
+{
+    auto req = response->getHttpRequest();
+    if (req->getUserData() == (void*)1)
+        return;
+    req->setUserData((void*)2);
+
+    if (!response)
+    {
+        callback(false, "no response");
+        return ;
+    }
+
+    if (!response->isSucceed())
+    {
+        callback(false, std::string(response->getErrorBuffer()));
+        return;
+    }
+
+    std::vector<char>* buffer = response->getResponseData();
+    callback(true, std::string(buffer->begin(), buffer->end()));
+}
+
+void NetManager::getJson(const std::string &url, int timeout, const std::function<void (bool, const std::string &)> &cb)
+{
+    HttpRequest* req = createJsonRequest(url, "");
+    if (timeout)
+    {
+        req->retain();
+        cocos2d::Director::getInstance()->getScheduler()->schedule([=](float dt){
+            if (req->getUserData() == nullptr)
+            {
+                req->setUserData((void*)1);
+                cb(false, "time out");
+            }
+            req->release();
+        }, req, 0, 0, timeout / 1000.0f, false, cocos2d::StringUtils::format("timeout_function %p", req));
+    }
+    req->setResponseCallback(std::bind(onReceiveJson, std::placeholders::_1, std::placeholders::_2, cb));
+    HttpClient::getInstance()->sendImmediate(req);
+    req->release();
+}
+
+END_NS_FY
---
+++ FengYun/frameworks/runtime-src/Classes/managers/NetManager.h
@@ -0,0 +1,147 @@
+/**
+ * @file  NetManager.h
+ * @project  FengYun
+ * @author Bluce Nong <nongjinxia@mokylin.com>
+ * @Created by BluceNong on 2017/2/13.
+ * @  Copyright © 2017年 MokyLin  Bluce Nong <nongjinxia@mokylin.com>. All rights reserved.
+*/
+
+#ifndef NETMANAGER_H
+#define NETMANAGER_H
+
+#include <functional>
+#include "Net/Timer.h"
+#include "gamebase.h"
+#include "Net/GameConnection.h"
+#include "Net/Decoder.h"
+#include "Net/Encoder.h"
+#include "GameModule.h"
+
+#define GAME_MSG_ID(modId, cmdId) (modId << 8 | cmdId)
+
+BEGIN_NS_FY
+
+class NetManager : public GameModule
+{
+public:
+    class Listener
+    {
+    public:
+        virtual ~Listener() {}
+
+        virtual void onConnected(const std::string& name) = 0;
+        virtual void onDisconnected(const std::string& name) = 0;
+    };
+
+    struct PrivateData;
+
+    NetManager();
+    ~NetManager();
+
+    void connect(const std::string& name, const std::string& ip, unsigned short port, const std::function<void(bool)>&cb);
+    void disconnect(const std::string& name);
+    void setLostCallback(const std::function<void()>& cb);
+
+    net::GameConnection* getGameConnection();
+
+    void registerListener(Listener* listener);
+    void unregisterListener(Listener* listener);
+
+    net::Timer* createTimer();
+
+    void getJson(const std::string& url, const std::function<void(bool succ, const std::string& respStr)>& cb)
+    { getJson(url, 0, cb); }
+
+    void getJson(const std::string& url, int timeout, const std::function<void(bool succ, const std::string& respStr)>& cb);
+
+private:
+    PrivateData* _data;
+};
+
+static const int SEND_HEAD_SIZE = 2 + 2 + 8;
+
+class GamePacketSendHelper
+{
+public:
+    inline GamePacketSendHelper(int64_t msgId, int pktSize)
+        : _msgId(msgId)
+        , _conn(GameModule::get<NetManager>()->getGameConnection())
+        , _pkt(net::PacketCache::makePacket(pktSize + SEND_HEAD_SIZE))
+        , _encoder(_pkt->buffer())
+    {
+        _encoder.writeUInt16(pktSize + SEND_HEAD_SIZE);
+        _encoder.writeUInt8(0);
+        _encoder.writeUInt8(0);
+        _encoder.writeUInt32((msgId >> 32) & 0xFFFFFFFF);
+        _encoder.writeUInt32(msgId & 0xFFFFFFFF);
+    }
+
+    inline ~GamePacketSendHelper()
+    {
+        if (_conn == nullptr) return;
+
+        if (_pkt->size() > _encoder.offset())
+        {
+            _conn->send(_msgId, net::PacketCache::makeSlice(_pkt, 0, _encoder.offset()));
+        }
+        else
+        {
+            _conn->send(_msgId, _pkt);
+        }
+    }
+
+    inline net::BasicEncoder& getEncoder() { return _encoder; }
+
+private:
+    int64_t _msgId;
+    net::GameConnection* _conn;
+    net::PacketPtr _pkt;
+    net::BasicEncoder _encoder;
+};
+
+class GameRPCSendHelper
+{
+public:
+    inline GameRPCSendHelper(int msgId, int pktSize)
+        : _conn(GameModule::get<NetManager>()->getGameConnection())
+        , _req(net::RPCRequest::create())
+        , _pkt(net::PacketCache::makePacket(pktSize))
+        , _encoder(_pkt->buffer())
+    {
+        _req->setMsgId(msgId);
+    }
+
+    inline ~GameRPCSendHelper()
+    {
+        if (_pkt->size() > _encoder.offset())
+        {
+            _req->setPacket(net::PacketCache::makeSlice(_pkt, 0, _encoder.offset()));
+        }
+        else
+        {
+            _req->setPacket(_pkt);
+        }
+        _conn->sendRPC(_req);
+    }
+
+    inline net::BasicEncoder& getEncoder() { return _encoder; }
+
+    inline void setCallback(const std::function<void(int error, const net::PacketPtr& pkt)>& cb)
+    {
+        _req->setCallback(cb);
+    }
+
+    inline void setCancelCallback(const std::function<void()>& cb)
+    {
+        _req->setCancelCallback(cb);
+    }
+
+private:
+    net::GameConnection* _conn;
+    net::RPCRequest* _req;
+    net::PacketPtr _pkt;
+    net::BasicEncoder _encoder;
+};
+
+END_NS_FY
+#endif /* NETMANAGER_H */
