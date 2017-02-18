45522a86f267a77a0f5e27aef695d244b2eaddf1
blucenong <351536411@163.com>
2017-02-18 16:59:45 +0800

    binding user data to lua.

--- FengYun/frameworks/runtime-src/Classes/lua/user.inc.hpp
+++ FengYun/frameworks/runtime-src/Classes/lua/user.inc.hpp
@@ -41,8 +41,83 @@ DEF_LUA_FUNC(updateServerList)
     RET_ARG_ERROR(updateServerList);
 }
 
+DEF_LUA_FUNC(connectServer)
+{
+    int serverId;
+    bool ok = true;
+    ok &= luaval_to_int32(L, 1, &serverId);
+    LUA_FUNCTION func = toluafix_ref_function(L, 2, 0);
+    if (ok && func)
+    {
+        LuaHandler h = createLuaHandler(func);
+        fy::GameModule::get<fy::UserManager>()->connectServer(serverId, [=](fy::UserManager::Result r){
+            GetLuaStack()->pushInt((int)r);
+            GetLuaStack()->executeFunctionByHandler(h->getHandler(), 1);
+        });
+        return 0;
+    }
+    RET_ARG_ERROR(connectServer);
+}
+
+DEF_LUA_FUNC(createUser)
+{
+    std::string name;
+    std::string password;
+    bool ok = true;
+    ok &= luaval_to_std_string(L, 1, &name);
+    ok &= luaval_to_std_string(L, 2, &password);
+    LUA_FUNCTION func = toluafix_ref_function(L, 3, 0);
+    if (ok && func)
+    {
+        LuaHandler h = createLuaHandler(func);
+        fy::GameModule::get<fy::UserManager>()->createUser(name, password, [=](fy::UserManager::Result r){
+            GetLuaStack()->pushInt((int)r);
+            GetLuaStack()->executeFunctionByHandler(h->getHandler(), 1);
+        });
+        return 0;
+    }
+    RET_ARG_ERROR(createUser);
+}
+
+DEF_LUA_FUNC(createRole)
+{
+    std::string name;
+    int job;
+    bool ok = true;
+    ok &= luaval_to_std_string(L, 1, &name);
+    ok &= luaval_to_int32(L, 2, &job);
+    LUA_FUNCTION func = toluafix_ref_function(L, 3, 0);
+    if (ok && func)
+    {
+        LuaHandler h = createLuaHandler(func);
+        fy::GameModule::get<fy::UserManager>()->createRole(name, (fy::RoleJob)job, [=](fy::UserManager::Result r){
+            GetLuaStack()->pushInt((int)r);
+            GetLuaStack()->executeFunctionByHandler(h->getHandler(), 1);
+        });
+        return 0;
+    }
+    RET_ARG_ERROR(createRole);
+}
+
+DEF_LUA_FUNC(setTestUser)
+{
+    std::string name;
+    bool ok = true;
+    ok &= luaval_to_std_string(L, 1, &name);
+    if (ok)
+    {
+        fy::GameModule::get<fy::UserManager>()->setTestUser(name);
+        return 0;
+    }
+    RET_ARG_ERROR(setTestUser);
+}
+
 inline static void registerUserFunctions(lua_State* L)
 {
     TOLUA_FUNC(getServers);
     TOLUA_FUNC(updateServerList);
+    TOLUA_FUNC(connectServer);
+    TOLUA_FUNC(createUser);
+    TOLUA_FUNC(setTestUser);
+    TOLUA_FUNC(createRole);
 }
