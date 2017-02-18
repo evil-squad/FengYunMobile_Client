8aaefa83e27f89436933ecc9d7e296c357dcf210
blucenong <351536411@163.com>
2017-02-18 18:31:11 +0800

    Add Result Enum to lua.

--- FengYun/tools/lua-enum-generator/game-enum.rb
+++ FengYun/tools/lua-enum-generator/game-enum.rb
@@ -8,6 +8,22 @@ Builder.define do
 	end
 
 	namespace "game" do
+		enum Result do
+      value SUCCESS
+      value NO_USER_ERROR
+      value USER_NAME_EXISTS
+      value WRONG_USER_PASSWORD
+      value USER_PROHIBIT
+      value NO_ROLE_ERROR
+      value ROLE_NAME_EXISTS
+      value NETWORK_ERROR
+      value UNKNOW_ERROR
+      value VERSION_DEBUG_ERROR
+      value VERSION_PROTOCOL_ERROR
+      value VERSION_PROTOCOL_WRONG
+      value VERSION_RESOURCE_WRONG
+    end
+    
 		enum InputEventType do
 			value AXIS_EVENT, 1
 			value BUTTON_EVENT
