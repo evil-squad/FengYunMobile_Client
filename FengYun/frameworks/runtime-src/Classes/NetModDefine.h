ad1db7ef3649ed60f2d5f5ca6bc97cfc6a0b0b9c
blucenong <351536411@163.com>
2017-02-18 18:26:16 +0800

    Add NetModDefine file.

---
+++ FengYun/frameworks/runtime-src/Classes/NetModDefine.h
@@ -0,0 +1,20 @@
+/**
+ * @file  NetModDefine.h
+ * @project  FengYun
+ * @author Bluce Nong <nongjinxia@mokylin.com>
+ * @Created by BluceNong on 2017/2/16.
+ * @  Copyright © 2017年 MokyLin  Bluce Nong <nongjinxia@mokylin.com>. All rights reserved.
+*/
+
+#ifndef NETMODDEFINE_H
+#define NETMODDEFINE_H
+
+#include "gamebase.h"
+
+BEGIN_NS_FY
+
+static const int LOGIN_MOD_ID = 1;
+static const int MISC_MODULE_ID = 2;
+
+END_NS_FY
+#endif /* NETMODDEFINE_H */
