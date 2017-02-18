669c42d5e54f2d15486abd090680433a47dd6951
blucenong <351536411@163.com>
2017-02-18 16:29:13 +0800

    Config Android Project File.

--- FengYun/frameworks/runtime-src/proj.android/jni/Android.mk
+++ FengYun/frameworks/runtime-src/proj.android/jni/Android.mk
@@ -10,12 +10,19 @@ FILE_LIST += $(wildcard $(LOCAL_PATH)/../../Classes/*cpp)
 FILE_LIST += $(wildcard $(LOCAL_PATH)/../../Classes/ECS/*.cpp)
 FILE_LIST += $(wildcard $(LOCAL_PATH)/../../Classes/FSM/*.cpp)
 FILE_LIST += $(wildcard $(LOCAL_PATH)/../../Classes/GUI/*.cpp)
-FILE_LIST += $(wildcard $(LOCAL_PATH)/../../Classes/Net/*cpp)
+FILE_LIST += $(wildcard $(LOCAL_PATH)/../../Classes/Net/*.cpp)
+FILE_LIST += $(wildcard $(LOCAL_PATH)/../../Classes/Net/Protobuf/*.cc)
 FILE_LIST += $(wildcard $(LOCAL_PATH)/../../Classes/Scenes/*.cpp)
 FILE_LIST += $(wildcard $(LOCAL_PATH)/../../Classes/lua/*.cpp)
 FILE_LIST += $(wildcard $(LOCAL_PATH)/../../Classes/lua/auto/*.cpp)
 FILE_LIST += $(wildcard $(LOCAL_PATH)/../../Classes/managers/*.cpp)
 FILE_LIST += $(wildcard $(LOCAL_PATH)/../../Classes/misc/*.cpp)
+FILE_LIST += $(wildcard $(LOCAL_PATH)/../../../external/protobuf/*.cc)
+FILE_LIST += $(wildcard $(LOCAL_PATH)/../../../external/protobuf/google/*.cc)
+FILE_LIST += $(wildcard $(LOCAL_PATH)/../../../external/protobuf/google/protobuf/*.cc)
+FILE_LIST += $(wildcard $(LOCAL_PATH)/../../../external/protobuf/google/protobuf/io/*.cc)
+FILE_LIST += $(wildcard $(LOCAL_PATH)/../../../external/protobuf/google/protobuf/stubs/*.cc)
+FILE_LIST += $(wildcard $(LOCAL_PATH)/../../../external/protobuf/google/protobuf/testing/*.cc)
 
 LOCAL_SRC_FILES := hellolua/main.cpp $(FILE_LIST:$(LOCAL_PATH)/%=%)
 
@@ -24,7 +31,10 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes \
 					$(LOCAL_PATH)/../../Classes/Scenes \
 					$(LOCAL_PATH)/../../Classes/misc \
 					$(LOCAL_PATH)/../../Classes/lua \
-					$(LOCAL_PATH)/../../../external/asio/include
+					$(LOCAL_PATH)/../../Classes/Net \
+					$(LOCAL_PATH)/../../Classes/Net/Protobuf \
+					$(LOCAL_PATH)/../../../external/asio/include \
+					$(LOCAL_PATH)/../../../external/protobuf
 
 
 # _COCOS_HEADER_ANDROID_BEGIN
