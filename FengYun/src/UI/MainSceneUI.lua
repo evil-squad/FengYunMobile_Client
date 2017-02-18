b069788dd125fe589ee28b9e274439763d5b0123
blucenong <351536411@163.com>
2017-02-18 18:28:44 +0800

    Add MainSceneUI.

---
+++ FengYun/src/UI/MainSceneUI.lua
@@ -0,0 +1,15 @@
+--
+-- Author: MokyLin Bluce Nong <nongjinxia@mokylin.com>
+-- Date: 2017-02-18 16:41:31
+--
+
+local function getRes(path)
+	return "mainScene/" .. path
+end
+
+
+function openMainSceneUI()
+	local uirect = game.getUIRect()
+	local uilayer = game.getUILayer()
+
+end
